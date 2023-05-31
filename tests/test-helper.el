3e5af7b4931b2c826955c994da4ddfab163cf163
hubisan <hubisan@gmail.com>
2023-05-31 21:45:35 +0200

    added test helping function: test-helper-get-file-in-test-dir

--- tests/test-helper.el
+++ tests/test-helper.el
@@ -12,6 +12,15 @@
     (insert-file-contents path)
     (buffer-string)))
 
+(defun test-helper-get-file-in-test-dir (file)
+  "Return the absolute path of the FILE located inside the testing directory.
+Example:
+  (test-helper-get-file-in-test-dir \"test.txt\")
+  Returns the absolute path to \"tests/test.txt\""
+  (let* ((pkg-dir (expand-file-name default-directory))
+         (tests-dir (expand-file-name "tests" pkg-dir)))
+    (expand-file-name file tests-dir)))
+
 (provide 'test-helper)
 
 ;;; test-helper.el ends here
